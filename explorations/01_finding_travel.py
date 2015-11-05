5be247ac737e0436a4c66f1cf549b23f0b80ae0e
seth wolfwood <seth@sethish.com>
2015-11-05 15:00:33 -0500

    Moves and cleanups ipy notebook formatting

--- explorations/01_finding_travel.py
+++ explorations/01_finding_travel.py
@@ -1,12 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# coding: utf-8
+import json
 
-# In[3]:
-
-import simplejson as json
-
-
-# In[15]:
 
 def load_cities():
     # loads 1000 cities from a corpora list
@@ -17,30 +13,14 @@ def load_cities():
     city_list = [city['city'] for city in us_cities['cities']]
     return city_list
 
-
-# In[16]:
-
 city_list = load_cities()
 print(len(city_list))
-
-
-# In[23]:
 
 # search for people driving to the top x cities in the USA
 def search_github(city_list):
     search_url_temp = "https://api.github.com/search/code?q=user:GITenberg+%22{0}+to+{1}%22"
     for city in city_list:
-        clean_city = city.
+        clean_city = city
         print(search_url_temp.format("drive", city))
-    
-
-
-# In[24]:
 
 search_github(city_list[:5])
-
-
-# In[ ]:
-
-"".
-
